0226538928adc8fb85edeac769e092fad6c740fe
MorganSchofield <morgan@wts-group.com>
2021-11-11 14:39:04 +0000

    Update routes.php

--- routes.php
+++ routes.php
@@ -2,8 +2,36 @@
 
 require __DIR__.'/includes/config.php';
 
-$url = $_SERVER['REQUEST_URI'];
-$slug = explode('/', $_SERVER['REQUEST_URI']);
-$slug = $slug[count($slug)-1];
-	
-include('404.php');
+if(strstr($url, 'product/')){
+
+    /*  this means if url has  `product/ ` in it - like http://localhost/ecomm/product/product-one  */
+    /*  then make your product details page in a new file product-details-page.php  */
+    /*  $slug will be available in product-details-page.php  */
+    /*  you don't need to add require config.php in product-details-page.php
+    because it's an included file and it will use the config included on this routes.php page.   */
+
+    $row = $productObj->getRowByFieldNotDeleted('seo_url', $slug);
+
+    if(!$row){
+
+    	include('404.php');
+    	exit;
+
+    }
+
+    include('product-details-page.php');
+    exit;
+
+} else{
+
+  	$row = $categoryObj->getRowByField('seo_url', $slug);
+
+    if(!$row){
+
+    	include('404.php');
+    	exit;
+
+    }
+  		include('product-list.php');
+
+}
