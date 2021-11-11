97995daa834971e049436f1f94dd6ac7e524dc51
MorganSchofield <morgan@wts-group.com>
2021-11-11 14:38:42 +0000

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,16 +4,18 @@ session_start();
 
 require __DIR__. '/../vendor/autoload.php';
 
-define('HOST', 'localhost');
-define('USERNAME', 'morgan');
-define('PASSWORD', 'MqY7qc7JCeeye8pU');
-define('DATABASE', 'ecomm');
+define('HOST', '213.171.200.101');
+define('USERNAME', 'morgan00231');
+define('PASSWORD', 'd8X!WwLb$*$tqU');
+define('DATABASE', 'ecommtest');
 define('DOMAIN', 'http://localhost/ecomm');
+
 define('SMTP_HOST', 'smtp.livemail.co.uk');
 define('COMPANY_NAME', 'Ecomm Test');
 define('SMTP_USERNAME', '');
 define('SMTP_EMAIL', '');
 define('SMTP_PASSWORD', '');
+
 define('SALT', 'yYdHfAJKicvO41R8SVqm3bHg6hWjkEFeUxPoXpGr7nDaMTC0IZLwulN5t9Q2rszB');
 define('SESSION', 'B9lMe1NKRJUC0EbSt7Tc8q4Gjx6kWOXordQ5fhmi2YVuznFHysIPprZAg3vaLHwD');
 date_default_timezone_set('Europe/London');
@@ -25,6 +27,7 @@ $cartObj = new App\Cart;
 $productObj = new App\Product;
 $categoryObj = new App\Category;
 $productImageObj = new App\ProductImage;
+$orderObj = new App\Order;
 
 
 if( !$user->uniqueId() ){
@@ -53,6 +56,9 @@ header('Location: '.$url); exit;
 
 }
 
+$url = $_SERVER['REQUEST_URI'];
+$slug = explode('/', $_SERVER['REQUEST_URI']);
+$slug = $slug[count($slug)-1];
 
 
 function dd($array){
