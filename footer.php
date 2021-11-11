bc817459b07294e3520c549c2e66b629c2f5e85f
MorganSchofield <morgan@wts-group.com>
2021-11-11 14:38:26 +0000

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,9 +1,16 @@
 
 
-<footer>
-
-
-
+<footer><br /><br /><br />
+<div style="bottom:0; position:relative; background-color:#f5f5f5;width:100%;padding:5px 0px 5px 0px; font-size:18px;">
+    <div class="row" style="">
+      <div class="col-md-6" style="text-align:center;">
+          Copyright 2021 <?= COMPANY_NAME ?>
+      </div>
+      <div class="col-md-6" style="text-align:center;">
+        Web Design By <a href="#">Desgin Co...</a>
+      </div>
+    </div>
+</div>
 </footer>
 
 </body>
