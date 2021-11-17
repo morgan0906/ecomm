34ebfc998a42e26a6de6a2c8b623785101ba9a14
MorganSchofield <morgan@wts-group.com>
2021-11-17 15:41:24 +0000

    Update product-list.php

--- product-list.php
+++ product-list.php
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 <?php
-  require('header.php');
-
   $query = $productObj->getAllByCategory($row->id);
+
+  require('header.php');
 ?>
 
 <div class="container">
@@ -17,16 +16,17 @@
 
         <div class="col-md-3 col-sm-3 col-xs-6">
 
-            <div class="thumbnail mb-0 mt-50 pt-0 pb-0">
-              <a href="<?= DOMAIN ?>/product/<?= $row->seo_url ?>">
-                <img style="width:100%" src="<?= DOMAIN ?>/product-images/<?= $image->id ?>.<?= $image->ext ?>" alt="<?= $image->alt ?>"/>
-              </a>
+            <div class="same-height item thumbnail mb-0 mt-50 pt-0 pb-0">
+                  <a href="<?= DOMAIN ?>/product/<?= $row->seo_url ?>">
+                    <img style="width:100%" src="<?= DOMAIN ?>/product-images/<?= $image->id ?>.<?= $image->ext ?>" alt="<?= $image->alt ?>"/>
+                  </a>
             </div>
-            <p style="text-align:center;margin-top:20px;padding-bottom:0px;margin-bottom:0px;font-size:22px;">
-                <a href="<?= DOMAIN ?>/product/<?= $row->seo_url ?>">
+
+            <a href="<?= DOMAIN ?>/product/<?= $row->seo_url ?>">
+                <p style="text-align:center;margin-top:20px;padding-bottom:0px;margin-bottom:0px;font-size:22px;">
                   <?= $row->title ?>
-                </a>
-            </p>
+                </p>
+            </a>
             <p style="text-align:center;font-weight:700;padding-top:0px;margin-top:0px;font-size:22px;">
                 <?php if($row->special_offer_price){
                   echo '<span style="color:red;padding-right:5px;"><strike>£'.$row->price.'</strike></span>£'.$row->special_offer_price;
@@ -58,21 +58,3 @@
 <?php
 require('footer.php');
 ?>
-=======
-<?php
-
-$query = $productObj->getAllByCategory($category_id);
-
-
-  require('header.php');
-
-?>
-
-<div class="container">
-  <h1><?= $title ?></h1>
-</div>
-
-<?php
-require('footer.php');
-?>
->>>>>>> f824e7cca0f346142aef68d1539110768725155e
