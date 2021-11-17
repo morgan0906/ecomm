48bbecb33f33d7acac65df90e0315e2e4489da31
MorganSchofield <morgan@wts-group.com>
2021-11-17 15:40:55 +0000

    Update Order.php

--- app/Order.php
+++ app/Order.php
@@ -9,7 +9,7 @@ class Order extends ObjectModel
 {
 
     protected $table = 'orders';
-    protected $fillable = ['user_id', 'shipping', 'cost', 'promo_code_id', 'discount_type', 'discount_amount'];
+    protected $fillable = ['user_id', 'shipping', 'cost', 'totalCost', 'promo_code_id', 'notes'];
     protected $user_id;
     protected $shipping;
     protected $cost;
@@ -29,88 +29,88 @@ class Order extends ObjectModel
 
     }
 
-	
+
     public function getAll($status)
     {
-	
+
 	if( $status == 'All' ){
-		
+
 		/*  IF CUSTOMER IS VIEWING ORDERS  */
-	
-	return $this->execute("SELECT *, orders.id AS order_id, orders.created_at AS order_date 
-					FROM orders WHERE user_id = ? AND deleted_at IS NULL AND status != 'Pending' 
+
+	return $this->execute("SELECT *, orders.id AS order_id, orders.created_at AS order_date
+					FROM orders WHERE user_id = ? AND deleted_at IS NULL AND status != 'Pending'
 					ORDER BY id DESC ", [$this->user->auth()->id]);
-	
+
 	} else {
-	
+
 		/*  IF ADMIN IS VIEWING ORDERS  */
-		
+
 		if($status == 'Dispatched'){
-		
-			return $this->execute("SELECT *, orders.id AS order_id, orders.created_at AS order_date 
-							FROM orders LEFT JOIN users ON users.id = orders.user_id 
+
+			return $this->execute("SELECT *, orders.id AS order_id, orders.created_at AS order_date
+							FROM orders LEFT JOIN users ON users.id = orders.user_id
 							WHERE orders.status = ? AND orders.deleted_at IS NULL
-							ORDER BY orders.dispatched_date DESC  ", [$status] );		
-		
+							ORDER BY orders.dispatched_date DESC  ", [$status] );
+
 		} else {
-		
-			return $this->execute("SELECT *, orders.id AS order_id, orders.created_at AS order_date 
-							FROM orders LEFT JOIN users ON users.id = orders.user_id 
+
+			return $this->execute("SELECT *, orders.id AS order_id, orders.created_at AS order_date
+							FROM orders LEFT JOIN users ON users.id = orders.user_id
 							WHERE orders.status = ? AND orders.deleted_at IS NULL
 							ORDER BY orders.id DESC  ", [$status] );
-		
+
 		}
 
 
 	}
 
     }
-    
-    
+
+
         public function getAllByEmailAddress($email)
     {
 
-	
-	return $this->execute("SELECT *, orders.id AS order_id, orders.created_at AS order_date FROM orders 
-					LEFT JOIN users ON users.id = orders.user_id WHERE users.email = ? 
-					AND orders.deleted_at IS NULL AND orders.status != 'Pending' 
+
+	return $this->execute("SELECT *, orders.id AS order_id, orders.created_at AS order_date FROM orders
+					LEFT JOIN users ON users.id = orders.user_id WHERE users.email = ?
+					AND orders.deleted_at IS NULL AND orders.status != 'Pending'
 					ORDER BY orders.id DESC ", [$email]);
 
     }
-    
-		
+
+
     public function search($search)
     {
 
-	
-	return $this->execute("SELECT *, orders.id AS order_id, orders.created_at AS order_date 
-					FROM orders LEFT JOIN users ON users.id = orders.user_id  WHERE 
-					( orders.order_number LIKE ? OR users.last_name LIKE ? OR users.email LIKE ? OR users.postcode LIKE ? OR users.phone LIKE ?  ) 
+
+	return $this->execute("SELECT *, orders.id AS order_id, orders.created_at AS order_date
+					FROM orders LEFT JOIN users ON users.id = orders.user_id  WHERE
+					( orders.order_number LIKE ? OR users.last_name LIKE ? OR users.email LIKE ? OR users.postcode LIKE ? OR users.phone LIKE ?  )
 					AND orders.deleted_at IS NULL ORDER BY orders.id DESC ", ["%".$search."%", "%".$search."%", "%".$search."%", "%".$search."%", "%".$search."%"]);
 
-    }		
-		
+    }
+
 
     public function getOrderNumber()
     {
 
 	if( isset($_COOKIE[SESSION.'order_number']) && $_COOKIE[SESSION.'order_number'] != '' && $_COOKIE[SESSION.'order_number'] != null ){
-	
+
 		return $_COOKIE[SESSION.'order_number'];
-	
+
 	}
-	
+
 	if( isset($_SESSION[SESSION.'order_number']) && $_SESSION[SESSION.'order_number'] != '' && $_SESSION[SESSION.'order_number'] != null ){
-	
+
 		return $_SESSION[SESSION.'order_number'];
-	
+
 	}
-	
-	return false;		
+
+	return false;
 
     }
-		
-		
+
+
     public function getOrderDescription()
     {
 
@@ -119,19 +119,19 @@ class Order extends ObjectModel
 	$i = 0;
 
 	foreach( $this->cart->getAll() as $row ){
-	
+
 		$description .= $i == 0 ? $row->quantity.' X '.$row->title.' '.str_replace(' <br /> ', '', $row->options) : ' : '.$row->quantity.' X '.$row->title.' '.str_replace(' <br /> ', '', $row->options);
-		
+
 	$i++;
-	
+
 	}
-	
+
 	return $description;
 
     }
 
 
-		
+
     public function setOrderToCompleted($order_number)
     {
 
@@ -142,150 +142,165 @@ class Order extends ObjectModel
 
     public function updateStock($order_number)
     {
-	
+
 	$this->id = $order_number - 100000;
-	
+
 	$query = $this->execute("SELECT * FROM products_from_order WHERE order_id = ? AND deleted_at IS NULL ", [$this->id]);
-	
+
 	$this->table = 'products';
-	
+
 	foreach( $query as $row ){
-	
+
 		$stock = $this->find($row->product_id)->qty_available;
 		$this->updateRow('products', ['qty_available' => ($stock - $row->quantity)], 'id = :id LIMIT 1 ', [ 'id' => $row->product_id ] );
-	
+
 	}
 
     }
-    
+
     public function replenishStock($order_number)
     {
-    
+
 	$this->id = $order_number - 100000;
-	
+
 	$query = $this->execute("SELECT * FROM products_from_order WHERE order_id = ? AND deleted_at IS NULL ", [$this->id]);
-	
+
 	$this->table = 'products';
-	
+
 	foreach( $query as $row ){
-	
+
 		$stock = $this->find($row->product_id)->qty_available;
 		$this->updateRow('products', ['qty_available' => ($stock + $row->quantity)], 'id = :id LIMIT 1 ', [ 'id' => $row->product_id ] );
 		$this->updateRow('orders', ['deleted_at' => DT], 'order_number = :order_number LIMIT 1 ', [ 'order_number' => $order_number ] );
-	
+
 	}
 
     }
 
-		
+
     public function sendDispatchedEmail($order_id)
     {
 
 	/* ROW WITH ORDER DETAILS */
-	
+
 	$order_row = $this->find($order_id);
-	
+
 	/* ROW WITH USER DETAILS */
-	
+
 	$user_row = $this->user->find($order_row->user_id);
-	
+
 	$html = "<br /><p>Dear ".ucwords($user_row->first_name).",<br /><br />
 	Your order ".$order_row->order_number." has been dispatched to;<br /><br />
 	".$user_row->first_name." ".$user_row->last_name."<br />
 	".$user_row->address_1."<br />";
-	
+
 	if($user_row->address_2){
-	
+
 		$html .= $user_row->address_2."<br />";
-	
+
 	}
-	
+
 	$html .= $user_row->town."<br />".$user_row->postcode."<br /><br />Your tracking code is: ".$order_row->tracking_code."<br /><br /><a href='https://www.royalmail.com/business/track-your-item#/'>https://www.royalmail.com/business/track-your-item#/</a><br /><br /></p>";
 
 
 	Mail::send(trim($user_row->email), $html, 'Order Dispatched - '.$order_row->order_number, COMPANY_NAME);
-	
+
 	return redirect( $_SERVER['HTTP_REFERER'], 'The dispatched email has been sent' );
 
     }
-		
-		
+
+    public function creationOfOrder()
+    {
+
+		$this->user_id = $this->user->add();
+		$this->shipping = $this->cart->shipping();
+		$this->cost = $this->cart->subTotal();
+    $this->totalCost = $this->cart->total();
+		$this->order_id = $this->add();
+		$order_number = $this->order_id + 50000;
+		$this->updateRow($this->table, ['order_number' => $order_number], 'id = :id', [ 'id' => $this->order_id ] );
+		$this->productsFromOrder->addOrderProducts($this->order_id);
+		return redirect( 'paypal' );
+
+    }
+
+
     public function createOrder()
     {
-    
+
 	/*
 
 	if(!isset($_POST['terms_check'])){
-	
+
 		return redirect( 'checkout.php', 'You must agree to our terms and conditions', 'e' );
-	
+
 	}
-	
+
 	*/
 
 	if( $this->user->auth() ){
-	
+
 		/* IF CUST IS LOGGED IN PASS THE RULES TO UNSET ON USER CLASS */
-	
+
 		$this->user->updateCustomerFromCheckout( array('password') );
-		
+
 		$this->user_id = $this->user->auth()->id;
-	
+
 	} else {
-	
+
 		$this->user_id = $this->user->add();
-		
+
 	}
-		
+
 
 		$this->shipping = $this->cart->shipping();
 		$this->cost = $this->cart->subTotal();
-		$this->promo_code_id = $this->cart->getPromoDiscountCodeId();		
+		$this->promo_code_id = $this->cart->getPromoDiscountCodeId();
 		$this->discount_type = $this->promo_code->getPromoType();
 		$this->discount_amount = $this->promo_code->getPromoCodeAmount();
 
 		$this->order_id = $this->add();
-		
+
 		$order_number = $this->order_id + 100000;
-		
+
 		$this->updateRow($this->table, ['order_number' => $order_number], 'id = :id', [ 'id' => $this->order_id ] );
 
 		$this->productsFromOrder->addOrderProducts($this->order_id);
-		
+
 		/*  UPDATE STOCK HERE  */
-		
+
 		$this->updateStock($order_number);
-		
+
 		$_SESSION[SESSION.'order_number'] = $order_number;
 		setcookie(SESSION.'order_number', $order_number, time()+7200, '/');
 
 		return redirect( 'paypal' );
 
     }
-		
-		
+
+
     public function updateStatus($id)
     {
-    
+
 	$redirect = $_SERVER['HTTP_REFERER'];
-    
+
 	$status = $this->find($id)->status;
-	
+
 	if( $_POST['status'] == 'Dispatched' && $status != 'Dispatched' ){
 
 		$redirect = 'account?page=orders&status=New';
 		$this->updateRow($this->table, ['dispatched_date' => DT], 'id = :id LIMIT 1 ', [ 'id' => $id ] );
-	
+
 	}
-	
+
 	if( $_POST['status'] != 'Dispatched' && $status == 'Dispatched' ){
 
 		$this->updateRow($this->table, ['dispatched_date' => NULL], 'id = :id LIMIT 1 ', [ 'id' => $id ] );
-	
+
 	}
 
 	$this->updateRow($this->table, ['status' => $_POST['status'], 'notes' => $_POST['notes'], 'tracking_code' => $_POST['tracking_code']], 'id = :id LIMIT 1 ', [ 'id' => $id ] );
-	
+
 	return redirect( $redirect, 'The order has been updated' );
 
     }
@@ -295,29 +310,29 @@ class Order extends ObjectModel
     {
 
 	$row = $this->find($id);
-	
+
 	if( $row->user_id !== $this->user->auth()->id ){
-		
+
 		return false;
-	
+
 	}
-	
+
 	if( $row->status == 'Pending' ){
-		
+
 		return false;
-	
+
 	}
-	
+
 	return true;
 
     }
-		
-		
+
+
     public function countOrders()
     {
 
 	$query = $this->execute("SELECT * FROM orders WHERE status != ? AND deleted_at IS NULL  ", ['Pending']);
-	
+
 	return count($query);
 
     }
@@ -327,24 +342,24 @@ class Order extends ObjectModel
     {
 
 	$countOrders = $this->countOrders();
-	
+
 	if( basename($_SERVER['SCRIPT_NAME']) == 'complete.php' ){
-		
+
 		$isZero = $countOrders % 100;
-	
+
 	} else {
-	
+
 		$isZero = ($countOrders+1) % 100;
-	
+
 	}
-	
+
 	if( !$isZero && $countOrders > 0 ){
-	
+
 		return true;
-	
+
 	}
-	
-	return false;			
+
+	return false;
 
     }
 
@@ -355,35 +370,35 @@ class Order extends ObjectModel
 	$subTotal = 0;
 
 	$query = $this->productsFromOrder->getAll($order_id);
-	
+
 	foreach( $query as $row ){
-	
+
 		$subTotal += ($row->quantity * $row->price * $row->discount);
-	
+
 	}
-	
+
 	/*  IF THERE HAS BEEN A PROMO CODE APPLIED  */
-	
+
 	if( $this->find($order_id)->promo_code_id ){
-	
+
 		$discount = $this->promo_code->find($this->find($order_id)->promo_code_id)->percentage;
-		
+
 		$discount = (100 - $discount) / 100;
-		
+
 		$subTotal = $subTotal * $discount;
-	
+
 	}
-	
+
 	return $subTotal;
 
     }
-		
+
 
     public function countPreviousOrders($order_id)
     {
 
 	$query = $this->execute("SELECT * FROM orders WHERE id < ? AND deleted_at IS NULL AND status != 'Pending'  ", [$order_id]);
-	
+
 	return addOrdinalNumberSuffix(count($query)+1);
 
     }
@@ -391,21 +406,21 @@ class Order extends ObjectModel
 
     public function discountedPriceFromCompleted($promo_code_id, $sub_total, $discount_type, $discount_amount)
     {
-    
+
 	if($promo_code_id){
-    
+
 		if($discount_type == 'percentage'){
-		
+
 			return Tools::showDiscountPricePercentage($sub_total, $discount_amount);
-		
+
 		} else {
-		
-			return Tools::showDiscountPriceValue($sub_total, $discount_amount);	
-		
+
+			return Tools::showDiscountPriceValue($sub_total, $discount_amount);
+
 		}
-		
+
 	}
-	
+
 		return false;
 
     }
@@ -414,8 +429,8 @@ class Order extends ObjectModel
     public function getAllByDateAndStatus($from, $to, $status)
     {
 
-	return $this->execute("SELECT *, orders.id AS order_id, orders.created_at AS order_date 
-					FROM orders LEFT JOIN users ON users.id = orders.user_id 
+	return $this->execute("SELECT *, orders.id AS order_id, orders.created_at AS order_date
+					FROM orders LEFT JOIN users ON users.id = orders.user_id
 					WHERE orders.created_at BETWEEN ? AND ? AND orders.status = ? AND orders.deleted_at IS NULL
 					ORDER BY orders.id DESC  ", [$from.' 00:00:00', $to.' 23:59:59', $status] );
 
@@ -424,23 +439,23 @@ class Order extends ObjectModel
 
     public function export($from, $to)
     {
-    
+
 	$from = Tools::formatDate($from);
 	$to = Tools::formatDate($to);
 
 	$fields = "Order Date,Name,Order Number,Status,Order Amount,Shipping,Order Details,Qty,Notes \n";
-	
+
 	foreach($this->getAllByDateAndStatus($from, $to, $_GET['status']) as $row){
-	
+
 	$notes = str_replace(array(",", "\n", "\r"), " ", strtoupper($row->notes));
 	$notes = str_replace('  ', ' ', $notes);
-	
+
 		foreach($this->productsFromOrder->getAll($row->order_id) as $product){
 
 		$fields .= date('d/m/Y', strtotime($row->order_date))." , ".str_replace(",", "", strtoupper($row->first_name))." ".str_replace(",", "", strtoupper($row->last_name))." , ".$row->order_number." , ".$row->status." , ".$row->cost." , ".$row->shipping." , ".str_replace(",", "", strtoupper($product->title))." , ".$product->quantity." , ".$notes."  \n";
-		
+
 		}
-	
+
 	}
 
 	header("Content-type: text/csv");
@@ -458,7 +473,7 @@ class Order extends ObjectModel
     {
 
 	$this->updateRow($this->table, ['deleted_at' => DT], 'id = :id  ', [ 'id' => $id ] );
-	
+
 	return redirect($_SERVER['HTTP_REFERER'], 'The order has been deleted');
 
     }
