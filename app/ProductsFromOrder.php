3c45fe5422692eab3008d412eaf5133f48791f22
MorganSchofield <morgan@wts-group.com>
2021-11-17 15:41:02 +0000

    Update ProductsFromOrder.php

--- app/ProductsFromOrder.php
+++ app/ProductsFromOrder.php
@@ -6,7 +6,7 @@ class ProductsFromOrder extends ObjectModel
 {
 
     protected $table = 'products_from_order';
-    protected $fillable = ['order_id', 'product_id', 'quantity', 'price', 'title', 'discount', 'options'];
+    protected $fillable = ['order_id', 'product_id', 'quantity', 'price', 'title', 'options'];
     protected $product_id;
     protected $quantity;
     protected $price;
@@ -24,23 +24,23 @@ class ProductsFromOrder extends ObjectModel
     {
 	return $this->execute("SELECT * FROM products_from_order WHERE products_from_order.order_id = ?  ", [$order_id] );
     }
-		
-		
+
+
     public function addOrderProducts($order_id)
     {
 
 	foreach( $this->cart->getAll() as $cart_row ){
-	
+
 		$this->order_id = $order_id;
 		$this->product_id = $cart_row->product_id;
 		$this->quantity = $cart_row->quantity;
 		$this->price = $cart_row->cart_price;
 		$this->title = $cart_row->title;
-		$this->discount = $cart_row->discount;
+		// $this->discount = $cart_row->discount;
 		$this->options = $cart_row->options;
-		
+
 		$this->add();
-	
+
 	}
 
     }
