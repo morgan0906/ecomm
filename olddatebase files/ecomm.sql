fd03dab0bb81269db3bb6ce3c5cb16701e9da30e
MorganSchofield <morgan@wts-group.com>
2021-11-25 12:38:00 +0000

    Create ecomm.sql

---
+++ olddatebase files/ecomm.sql
@@ -0,0 +1,353 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 20, 2021 at 11:56 AM
+-- Server version: 10.1.9-MariaDB
+-- PHP Version: 5.5.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ecomm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `cart_price` decimal(10,2) NOT NULL,
+  `unique_id` varchar(250) NOT NULL,
+  `options` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `seo_url` varchar(50) NOT NULL,
+  `meta_description` text,
+  `ext` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `title`, `seo_url`, `meta_description`, `ext`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Menswear', 'menswear', NULL, 'jpg', '2021-10-12 12:56:47', '2021-10-12 12:56:47', NULL),
+(2, 'Second', 'second', NULL, NULL, '2021-10-12 22:20:11', '2021-10-12 22:20:11', NULL),
+(3, 'Third', 'third', NULL, NULL, '2021-10-13 11:57:15', '2021-10-13 11:57:15', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `gallery_images`
+--
+
+CREATE TABLE `gallery_images` (
+  `id` int(11) NOT NULL,
+  `alt` varchar(256) DEFAULT NULL,
+  `ext` varchar(4) NOT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `status` varchar(10) NOT NULL DEFAULT 'Pending',
+  `shipping` decimal(10,2) NOT NULL,
+  `cost` decimal(10,2) NOT NULL,
+  `order_number` varchar(20) DEFAULT NULL,
+  `is_dispatched` int(11) DEFAULT NULL,
+  `promo_code_id` int(11) DEFAULT NULL,
+  `discount_type` varchar(10) DEFAULT NULL,
+  `discount_amount` decimal(10,2) DEFAULT NULL,
+  `notes` varchar(1000) DEFAULT NULL,
+  `dispatched_date` datetime DEFAULT NULL,
+  `tracking_code` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `category_id` varchar(255) DEFAULT NULL,
+  `sub_category_id` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `seo_url` varchar(100) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `qty_available` int(11) DEFAULT '10',
+  `special_offer_price` decimal(10,2) DEFAULT NULL,
+  `description` text,
+  `attributes` varchar(255) DEFAULT NULL,
+  `best_seller` int(11) NOT NULL DEFAULT '0',
+  `featured` int(11) NOT NULL DEFAULT '0',
+  `meta_description` text,
+  `weight` decimal(10,2) DEFAULT NULL,
+  `product_code` varchar(255) DEFAULT NULL,
+  `cross_sell_ids` varchar(255) DEFAULT NULL,
+  `sort_order` decimal(10,2) DEFAULT NULL,
+  `sale` int(11) NOT NULL DEFAULT '0',
+  `new` int(11) NOT NULL DEFAULT '0',
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `barcode` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `category_id`, `sub_category_id`, `title`, `seo_url`, `price`, `qty_available`, `special_offer_price`, `description`, `attributes`, `best_seller`, `featured`, `meta_description`, `weight`, `product_code`, `cross_sell_ids`, `sort_order`, `sale`, `new`, `created_at`, `updated_at`, `deleted_at`, `barcode`) VALUES
+(1, '["1"]', NULL, 'test', 'test', '10.00', NULL, NULL, NULL, NULL, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, '2021-10-12 13:01:52', '2021-10-12 13:06:12', '2021-10-12 13:06:12', NULL),
+(2, '["1","2"]', NULL, 'Product 1', 'product-1', '10.00', 10, NULL, NULL, 'null', 1, 1, NULL, NULL, NULL, NULL, NULL, 1, 1, '2021-10-12 13:02:31', '2021-10-13 11:46:06', NULL, NULL),
+(3, '["1","2"]', NULL, 'Product 2', 'product-2', '1.00', 10, NULL, NULL, 'null', 1, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, '2021-10-13 11:42:37', '2021-10-18 12:29:31', NULL, NULL),
+(4, '["1","2"]', NULL, 'Product 3', 'product-3', '10.50', 10, NULL, NULL, 'null', 1, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, '2021-10-13 11:45:25', '2021-10-13 14:11:32', NULL, NULL),
+(5, '["1","2"]', NULL, 'Product 4', 'product-4', '1.00', 10, NULL, NULL, 'null', 1, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, '2021-10-13 11:45:55', '2021-10-13 14:10:09', NULL, NULL),
+(6, '["1","2"]', NULL, 'Product 5', 'product-5', '1.00', 15, NULL, NULL, 'null', 1, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, '2021-10-13 11:46:26', '2021-10-18 12:29:16', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products_from_order`
+--
+
+CREATE TABLE `products_from_order` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `discount` decimal(10,2) NOT NULL,
+  `options` varchar(255) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_images`
+--
+
+CREATE TABLE `product_images` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `alt` varchar(50) DEFAULT NULL,
+  `ext` varchar(4) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `product_images`
+--
+
+INSERT INTO `product_images` (`id`, `product_id`, `alt`, `ext`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 1, NULL, 'jpg', '2021-10-12 13:01:52', '2021-10-12 13:01:52', NULL),
+(2, 2, NULL, 'jpg', '2021-10-12 13:02:31', '2021-10-13 11:46:06', NULL),
+(3, 3, NULL, 'jpg', '2021-10-13 11:42:37', '2021-10-13 11:42:51', '2021-10-13 11:42:51'),
+(4, 3, NULL, 'jpg', '2021-10-13 11:43:03', '2021-10-18 12:29:31', NULL),
+(5, 4, NULL, 'jpg', '2021-10-13 11:45:25', '2021-10-13 14:11:32', NULL),
+(6, 5, NULL, 'jpg', '2021-10-13 11:45:55', '2021-10-13 14:10:09', NULL),
+(7, 6, NULL, 'jpg', '2021-10-13 11:46:26', '2021-10-18 12:29:16', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `user_role_id` int(11) NOT NULL DEFAULT '1',
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(64) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `address_1` varchar(255) DEFAULT NULL,
+  `address_2` varchar(255) DEFAULT NULL,
+  `town` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `postcode` varchar(255) DEFAULT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `user_role_id`, `email`, `password`, `phone`, `address_1`, `address_2`, `town`, `country`, `postcode`, `remember_token`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Admin', 'User', 2, 'admin', '9a56681567a53c4a3d6581ce0d32dc187cf08c59634d26afa28ac56a94c3997e', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_roles`
+--
+
+CREATE TABLE `user_roles` (
+  `id` int(11) NOT NULL,
+  `role` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `gallery_images`
+--
+ALTER TABLE `gallery_images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products_from_order`
+--
+ALTER TABLE `products_from_order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product_images`
+--
+ALTER TABLE `product_images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `gallery_images`
+--
+ALTER TABLE `gallery_images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `products_from_order`
+--
+ALTER TABLE `products_from_order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `product_images`
+--
+ALTER TABLE `product_images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
